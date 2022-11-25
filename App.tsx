6c3e80bd6ef36d78aaf4d9c03a8e106e570c6ae4
ulrichstark <github@ustark.de>
2022-11-25 17:31:44 +0100

    add map and user location

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+import { requestForegroundPermissionsAsync } from "expo-location";
+import { StyleSheet, View } from "react-native";
+import { useEffect } from "react";
 
 export default function App() {
+    useEffect(() => {
+        requestForegroundPermissionsAsync();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <MapView style={styles.map} showsUserLocation followsUserLocation />
         </View>
     );
 }
@@ -13,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+    },
+    map: {
+        width: "100%",
+        height: "100%",
     },
 });
