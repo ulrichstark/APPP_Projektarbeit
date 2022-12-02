8db2dd5adb9d59e91709af7ead8aa12515c5e756
ulrichstark <github@ustark.de>
2022-12-02 10:48:30 +0100

    improve location accuracy and fix warning

--- src/hooks/useUserCoords.ts
+++ src/hooks/useUserCoords.ts
@@ -1,28 +1,42 @@
-import { requestForegroundPermissionsAsync, startLocationUpdatesAsync, LocationObject } from "expo-location";
+import { requestForegroundPermissionsAsync, startLocationUpdatesAsync, LocationObject, LocationAccuracy } from "expo-location";
 import { useEffect, useState } from "react";
 import { Coordinate } from "../models/Coordinate";
-import { defineTask } from "expo-task-manager";
+import { defineTask, TaskManagerTaskExecutor } from "expo-task-manager";
 import { config } from "../config";
+
+let executors: TaskManagerTaskExecutor[] = [];
+
+defineTask(config.taskLocationUpdates, (body) => {
+    for (const executor of executors) {
+        executor(body);
+    }
+});
 
 export function useUserCoords() {
     const [userCoords, setUserCoords] = useState<Coordinate | null>(null);
 
     useEffect(() => {
-        requestForegroundPermissionsAsync().then(() => {
-            defineTask(config.taskLocationUpdates, ({ data, error }) => {
-                if (error) {
-                    // check error.message for more details.
-                    return;
-                } else {
-                    const locations: LocationObject[] = (data as any).locations;
-                    if (locations) {
-                        setUserCoords(locations[0].coords);
-                    }
+        const executor: TaskManagerTaskExecutor = ({ data, error }) => {
+            if (error) {
+                // check error.message for more details.
+                return;
+            } else {
+                const locations: LocationObject[] = (data as any).locations;
+                if (locations) {
+                    setUserCoords(locations[0].coords);
                 }
-            });
+            }
+        };
 
-            startLocationUpdatesAsync(config.taskLocationUpdates);
+        executors.push(executor);
+
+        requestForegroundPermissionsAsync().then(() => {
+            startLocationUpdatesAsync(config.taskLocationUpdates, { accuracy: LocationAccuracy.BestForNavigation });
         });
+
+        return () => {
+            executors = executors.filter((e) => e !== executor);
+        };
     }, []);
 
     return userCoords;
