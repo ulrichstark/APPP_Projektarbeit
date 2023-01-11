562cddc4c53035f3330f77382621f62caaee27b6
Ulrich Stark <github@ustark.de>
2023-01-11 11:20:46 +0100

    pin-color depending on free spaces in parking-lot

--- src/components/ParkingLotMarker.tsx
+++ src/components/ParkingLotMarker.tsx
@@ -11,12 +11,18 @@ interface Props {
 
 export function ParkingLotMarker(props: Props) {
     const { parkingLot, onCalloutPress } = props;
+    const { free, total, name } = parkingLot;
+
+    // Grün: mehr als 20% frei
+    // Gelb: weniger als 20% frei
+    // Rot: nichts frei
+    const pinColor = free && total ? (free / total > 0.2 ? "green" : "yellow") : "red";
 
     return (
-        <Marker coordinate={parkingLot}>
+        <Marker coordinate={parkingLot} pinColor={pinColor}>
             <Callout onPress={onCalloutPress}>
                 <View style={styles.callout}>
-                    <Subheading style={{ color: "#08e" }}>{parkingLot.name}</Subheading>
+                    <Subheading style={{ color: "#08e" }}>{name}</Subheading>
                     <ParkingLotDescription parkingLot={parkingLot} />
                 </View>
             </Callout>
